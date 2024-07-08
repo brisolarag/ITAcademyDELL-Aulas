18f1900c7fa1eacf337588e7ecdd1d0f00f6e409
Gabriel Brisolara <dev.brisolara@gmail.com>
2024-07-08 08:43:55 -0300

    removed unused folders

--- modulo4/projeto_api_ef/Controllers/Categorias/CategoriaDto.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace projeto_api_ef.Controllers.Categorias
-{
-    public record CategoriaDto(string nome);
-}
