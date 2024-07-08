3010256ec4f0375e516fd3dad6d2417a5988612d
Gabriel Brisolara <dev.brisolara@gmail.com>
2024-07-08 09:24:31 -0300

    mod4: DemoConsoleSqlServer: String de conexao e onmodelcreating

--- modulo4/aula26.06/DemoConsoleSqlServer/Models/TarefaContext.cs
+++ modulo4/aula26.06/DemoConsoleSqlServer/Models/TarefaContext.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace DemoConsoleSqlServer.Models;
 public class TarefaContext : DbContext
 {
      public DbSet<Tarefa> Tarefas { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer(@"Server=win-brisolara;Database=ita.mod4.tarefasbd;TrustedConnection=True");
+        optionsBuilder.EnableSensitiveDataLogging().LogTo(Console.WriteLine, LogLevel.Information);
+        // string de script
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Tarefa>()
+                .Property(t => t.Nome)
+                .HasMaxLength(30);
+    }
 }
